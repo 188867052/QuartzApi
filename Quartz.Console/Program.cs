edd73a7d90151749ffff608aa33c6c96ec6c17a1
召 程 <542153354@qq.com>
2019-11-19 16:49:28 +0800

    add job

---
+++ Quartz.Console/Program.cs
@@ -0,0 +1,66 @@
+﻿using log4net;
+using Quartz;
+using Quartz.Console.Jobs;
+using Quartz.Impl;
+using Quartz.Logging;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace Quartz.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+            RunProgramRunExample().GetAwaiter().GetResult();
+            System.Console.WriteLine("Press any key to close the application");
+            System.Console.ReadKey();
+        }
+
+        public static async Task RunProgramRunExample()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(1)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                System.Console.WriteLine(se);
+            }
+        }
+    }
+}
