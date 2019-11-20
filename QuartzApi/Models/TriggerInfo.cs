50a17379776549917b6eb0c55d536640483c2155
召 程 <542153354@qq.com>
2019-11-20 11:19:52 +0800

    a

--- QuartzApi/Models/TriggerInfo.cs
+++ QuartzApi/Models/TriggerInfo.cs
@@ -25,31 +25,58 @@ namespace Quartz.Api.Models
         public string SchedName { get; set; }
 
         /// <summary>
-        /// TriggerName.
+        /// 触发器名称.
         /// </summary>
         public string TriggerName { get; set; }
 
         /// <summary>
-        /// TriggerGroup
+        /// 触发器组.
         /// </summary>
         public string TriggerGroup { get; set; }
 
+        /// <summary>
+        /// Job名称
+        /// </summary>
         public string JobName { get; set; }
 
+        /// <summary>
+        /// Job组.
+        /// </summary>
         public string JobGroup { get; set; }
 
+        /// <summary>
+        /// 下次触发时间.
+        /// </summary>
         public DateTime? NextFireTime { get; set; }
 
+        /// <summary>
+        /// 上次触发时间
+        /// </summary>
         public DateTime? PrevFireTime { get; set; }
 
+        /// <summary>
+        /// 优先级.
+        /// </summary>
         public int? Priority { get; set; }
 
+        /// <summary>
+        /// 触发器状态
+        /// </summary>
         public string TriggerState { get; set; }
 
+        /// <summary>
+        /// 触发器类型
+        /// </summary>
         public string TriggerType { get; set; }
 
+        /// <summary>
+        /// 开始时间
+        /// </summary>
         public DateTime? StartTime { get; set; }
 
+        /// <summary>
+        /// 结束时间
+        /// </summary>
         public DateTime? EndTime { get; set; }
     }
 }
