08b00d818f726de872130376378488b7bedba9f7
召 程 <542153354@qq.com>
2019-11-20 09:42:17 +0800

    q

---
+++ QuartzApi/Entities/QrtzCronTriggers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public partial class QrtzCronTriggers
+    {
+        public string SchedName { get; set; }
+
+        public string TriggerName { get; set; }
+
+        public string TriggerGroup { get; set; }
+
+        public string CronExpression { get; set; }
+
+        public string TimeZoneId { get; set; }
+
+        public virtual QrtzTriggers QrtzTriggers { get; set; }
+    }
+}
--- Shared/Connection.cs
+++ Shared/Connection.cs
@@ -6,36 +6,6 @@ namespace EFCore.Scaffolding.Extension
 {
     public static class Connection
     {
-        private static string aliCloud;
-        private const string LocalConnectionString = @"Data Source=.;Initial Catalog=quartz;Integrated Security=True";
-
-        public static string ConnectionString
-        {
-            get
-            {
-                if (!string.IsNullOrEmpty(aliCloud))
-                {
-                    return aliCloud;
-                }
-
-                string filePath = GetPipelineFile();
-                bool isAzure = !string.IsNullOrEmpty(filePath);
-                if (isAzure)
-                {
-                    aliCloud = File.ReadAllText(filePath);
-                    return aliCloud;
-                }
-                else
-                {
-                    return LocalConnectionString;
-                }
-            }
-        }
-
-        private static string GetPipelineFile()
-        {
-            var di = new DirectoryInfo(Environment.CurrentDirectory);
-            return Directory.GetFiles(di.Parent.Parent.Parent.Parent.Parent.FullName, "ConnectionString.txt", SearchOption.AllDirectories).FirstOrDefault();
-        }
+        public static string ConnectionString= @"Data Source=.;Initial Catalog=quartz;Integrated Security=True";
     }
 }
