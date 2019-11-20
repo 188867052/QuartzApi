a93e89b31d2de20b3e8237a16aff35d2e623b647
召 程 <542153354@qq.com>
2019-11-20 09:47:47 +0800

    a

--- QuartzApi/Startup.cs
+++ QuartzApi/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,14 @@ namespace QuartzApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEntityFrameworkSqlServer()
+                    .AddDbContext<QuartzDbContext>(options =>
+                    {
+                        //读取配置文件中的链接字符串
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServer"),
+                        //配置分页 使用旧方式
+                            b => b.UseRowNumberForPaging());
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
